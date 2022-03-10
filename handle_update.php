bb95a69b7a26f703bb47abb8e2b93a63f019329e
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-03-11 02:34:53 +0700

    123

--- handle_update.php
+++ handle_update.php
@@ -4,17 +4,20 @@
 
         $id = $_GET['id'];
         $productName = $_POST['name'];
-        $company = $_POST['company'];
-        $amount = $_POST['amount'];
         $image = $_POST['image'];
         $category = $_POST['category'];
+        $company = $_POST['company'];
+        $amount = $_POST['amount'];
         $price = $_POST['price'];
         
         $pg = "UPDATE product
-        SET name = '$productName', image= '$image', category =' $category', price='$price' , amount='$amount', company='$company' 
-        WHERE id = $id";
+        SET name = '$productName', image= '$image', category =' $category', price='$price' , company='$company', amount='$amount' WHERE id = $id";
 
         $result = pg_query($conn, $pg);
+        if(isset($result)) {
+            echo "oke";
+        }
+        else {echo "ngu";}
         header('location: main.php');
   
 
