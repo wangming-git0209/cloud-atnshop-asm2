7135655158d74eef66cb595abdd2f34c15577c4e
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-03-14 00:24:43 +0700

    123

--- shop.php
+++ shop.php
@@ -1,6 +1,6 @@
 <?php 
     include('database.php');
-    $username = $_POST['username'];
+    $username = $GET['username'];
     $sql = "SELECT * FROM product WHERE company= '$username' ";
     $result = pg_query($conn, $sql);
 ?>
