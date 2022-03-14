d3224ad59c852c59c8c595c9e2e07c64c772e5a0
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-03-14 15:29:24 +0700

    ádasd

--- handle_select.php
+++ handle_select.php
@@ -1,23 +1,42 @@
 <?php
 session_start();
 include 'database.php';
+if(isset($_SESSION['selectShop']) && isset($_SESSION['selectAllShop'])) 
+        { 
+            if ($_POST['submit']) { 
+                $company = $_POST['company'];
+                if($_POST['company'] == "allShop") {
+                    $_SESSION['selectAllShop'] = $company;
+                    $query = "SELECT * FROM product";
+                    unset($_SESSION['selectShop']);
+                    header("location: main.php"); 
+                    
+                }
+                else { 
+                    $query = "SELECT * FROM product where company='$company'";
 
-
-    if($_POST['submit']) {
-        $company = $_POST['company'];
-        
-        if($_POST['company'] == "allShop") { 
-            $_SESSION['selectAllShop'] = $company;
-            $query = "SELECT * FROM product";
-            header("location: main.php");
+                    $_SESSION['selectShop'] = $company;
+                    unset($_SESSION['selectAllShop']);
+                    header("location: main.php");
+                }
+            }
         }
-        else {      
-            $query = "SELECT * FROM product where company='$company'";
 
-            $_SESSION['selectShop'] = $company;
-            header("location: main.php");
-        }
+    // if($_POST['submit']) {
+    //     $company = $_POST['company'];
+
+    //     if($_POST['company'] == "allShop") { 
+    //         $_SESSION['selectAllShop'] = $company;
+    //         $query = "SELECT * FROM product";
+    //         header("location: main.php");
+    //     }
+    //     else {      
+    //         $query = "SELECT * FROM product where company='$company'";
+
+    //         $_SESSION['selectShop'] = $company;
+    //         header("location: main.php");
+    //     }
         
         
-    }
+    // }
 ?>
