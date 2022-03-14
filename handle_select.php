3d532d023fd21e1488e68fd0500cd1eb8b049d33
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-03-14 17:27:05 +0700

    Update handle_select.php

--- handle_select.php
+++ handle_select.php
@@ -58,26 +58,8 @@ if ($_POST['submit']) {
         }
 
         
-            if(isset($_SESSION['selectShop'])){
-                while($row = pg_fetch_assoc($result_select)) {
-                    ?>  
-                        <tr>
-                       
-                        <td><?php echo $row['name']?></td>
-                        <td><?php echo $row['company']?></td>
-                        <td><img src="./img/<?php echo $row['image']?>" alt="img" width="250" height="250"></td>
-                        <td><?php echo $row['category']?></td>
-                        <td><?php echo $row['amount']?></td>
-                        <td><?php echo $row['price']?></td>
-                        <td>
-                        <a href="update.php?id=<?php echo $row['id']?>" class="btn btn-success">Update </a>
-                        <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">Delete </a>
-                        </td>
-                    </tr> 
-                    <?php
-            }
-        }
-            ?>
+            
+    ?>
         
             
 
