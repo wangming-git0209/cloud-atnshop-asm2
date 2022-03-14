73c3104b384ac40380566f4ef024221b1634be0a
wangming-git0209 <minhnnqgcd191322@fpt.edu.vn>
2022-03-14 15:32:23 +0700

    ádas

--- handle_select.php
+++ handle_select.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 include 'database.php';
-if(isset($_SESSION['selectShop']) && isset($_SESSION['selectAllShop'])) 
+
+if ($_POST['submit']) { 
+    if(isset($_SESSION['selectShop']) && isset($_SESSION['selectAllShop'])) 
         { 
-            if ($_POST['submit']) { 
                 $company = $_POST['company'];
                 if($_POST['company'] == "allShop") {
                     $_SESSION['selectAllShop'] = $company;
@@ -22,21 +23,19 @@ if(isset($_SESSION['selectShop']) && isset($_SESSION['selectAllShop']))
             }
         }
 
-    // if($_POST['submit']) {
-    //     $company = $_POST['company'];
+    if($_POST['submit']) {
+        $company = $_POST['company'];
 
-    //     if($_POST['company'] == "allShop") { 
-    //         $_SESSION['selectAllShop'] = $company;
-    //         $query = "SELECT * FROM product";
-    //         header("location: main.php");
-    //     }
-    //     else {      
-    //         $query = "SELECT * FROM product where company='$company'";
+        if($_POST['company'] == "allShop") { 
+            $_SESSION['selectAllShop'] = $company;
+            $query = "SELECT * FROM product";
+            header("location: main.php");
+        }
+        else {      
+            $query = "SELECT * FROM product where company='$company'";
 
-    //         $_SESSION['selectShop'] = $company;
-    //         header("location: main.php");
-    //     }
-        
-        
-    // }
+            $_SESSION['selectShop'] = $company;
+            header("location: main.php");
+        } 
+    }
 ?>
